e26157ba507165746e03e673471edc83baefef38
Alex Altair <alexanderaltair@gmail.com>
2014-03-18 15:44:27 -0700

    Stop duplicating structs.

--- linked_list.c
+++ linked_list.c
@@ -5,16 +5,13 @@ typedef struct node{
     int value;
 } Node;
 
+void init_node(Node * node){
+    node->next = NULL;
+    node->value = 0;
+}
 void print_node(Node * node){
     printf("#<Node: value=%d, next=%p >\n", node->value, node->next);
 }
-
-Node new_node(){
-    Node node;
-    node.value = 0;
-    return node;
-}
-
 
 
 typedef struct linked_list{
@@ -22,28 +19,32 @@ typedef struct linked_list{
     int size;
 } LinkedList;
 
-LinkedList new_linked_list(){
-    LinkedList list;
-    list.root = NULL;
-    list.size = 0;
-    return list;
+void init_linked_list(LinkedList * list){
+    list->root = NULL;
+    list->size = 0;
 }
 
-void print_linked_list(LinkedList * linked_list){
+void print_linked_list(LinkedList * list){
     printf("[\n");
-    if (linked_list->root != NULL){
-        print_node(linked_list->root);
+    if (list->root != NULL){
+        printf("  root: ");
+        print_node(list->root);
     }
     printf("]\n");
 }
 
 
+
 int main(){
-    Node my_node = new_node();
+    Node my_node;
+    print_node(&my_node);
+    init_node(&my_node);
     my_node.value = 12;
     print_node(&my_node);
 
-    LinkedList my_linked_list = new_linked_list();
+    LinkedList my_linked_list;
+    init_linked_list(&my_linked_list);
+    print_linked_list(&my_linked_list);
     my_linked_list.root = &my_node;
     print_linked_list(&my_linked_list);
 
