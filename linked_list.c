dbb553358ee65e7c9b03d2bae97dfab9a5c4ff70
Alex Altair <alexanderaltair@gmail.com>
2014-03-18 14:51:04 -0700

    Add linked list version 2, with printing.

--- linked_list.c
+++ linked_list.c
@@ -5,23 +5,47 @@ typedef struct node{
     int value;
 } Node;
 
-void print_node(Node node){
-    printf("#<Node: value=%d, next=%p >\n", node.value, node.next);
+void print_node(Node * node){
+    printf("#<Node: value=%d, next=%p >\n", node->value, node->next);
+}
+
+Node new_node(){
+    Node node;
+    node.value = 0;
+    return node;
 }
 
 
+
 typedef struct linked_list{
-    Node root;
+    Node * root;
     int size;
 } LinkedList;
 
-int main(){
-    Node new_node = {.value = 12};
-    print_node(new_node);
+LinkedList new_linked_list(){
+    LinkedList list;
+    list.root = NULL;
+    list.size = 0;
+    return list;
+}
 
-    LinkedList new_list;
-    // printf("%d\n", new_list.root);
-    printf("%d\n", new_list.size);
+void print_linked_list(LinkedList * linked_list){
+    printf("[\n");
+    if (linked_list->root != NULL){
+        print_node(linked_list->root);
+    }
+    printf("]\n");
+}
+
+
+int main(){
+    Node my_node = new_node();
+    my_node.value = 12;
+    print_node(&my_node);
+
+    LinkedList my_linked_list = new_linked_list();
+    my_linked_list.root = &my_node;
+    print_linked_list(&my_linked_list);
 
     return 0;
 }
