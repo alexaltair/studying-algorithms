701fe290104f4c43f096016d108d79e2a1e0a6ea
Alex Altair <alexanderaltair@gmail.com>
2014-03-18 18:14:06 -0700

    Allocate Nodes on the heap & implement shift & unshift.

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 typedef struct node{
@@ -9,8 +10,9 @@ void init_node(Node * node){
     node->next = NULL;
     node->value = 0;
 }
+
 void print_node(Node * node){
-    printf("#<Node: value=%d, next=%p >\n", node->value, node->next);
+    printf("#<Node: address=%p, value=%d, next=%p>\n", node, node->value, node->next);
 }
 
 
@@ -29,24 +31,54 @@ void print_linked_list(LinkedList * list){
     if (list->root != NULL){
         printf("  root: ");
         print_node(list->root);
+        Node * node = (list->root)->next;
+        while(node != NULL){
+            printf("  next: ");
+            print_node(node);
+            node = node->next;
+        }
     }
     printf("]\n");
+}
+
+void shift(LinkedList * list){
+    Node * root = list->root;
+    if (root != NULL){
+        list->root = root->next;
+        list->size--;
+        free(root);
+    }
+}
+
+void unshift(LinkedList * list, int value){
+    Node * node = malloc(sizeof(Node));
+    node->value = value;
+    Node * temp = list->root;
+    node->next = temp;
+    list->root = node;
+    list->size++;
 }
 
 
 
 int main(){
-    Node my_node;
-    print_node(&my_node);
-    init_node(&my_node);
-    my_node.value = 12;
-    print_node(&my_node);
-
     LinkedList my_linked_list;
     init_linked_list(&my_linked_list);
+
+    unshift(&my_linked_list, 12);
+    unshift(&my_linked_list, 3);
+    unshift(&my_linked_list, 15);
+
     print_linked_list(&my_linked_list);
-    my_linked_list.root = &my_node;
+    printf("size: %d\n", my_linked_list.size);
+
+    shift(&my_linked_list);
     print_linked_list(&my_linked_list);
+    printf("size: %d\n", my_linked_list.size);
+
+    unshift(&my_linked_list, 53);
+    print_linked_list(&my_linked_list);
+    printf("size: %d\n", my_linked_list.size);
 
     return 0;
 }
